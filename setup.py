2d1db3140cc65d2aa461c711c86ac0f4ec00349b
Christian Mauceri <cmauceri@gmail.com>
2024-11-27 18:43:12 +0100

    Première fois

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+
+from setuptools import find_packages, setup
+
+
+def exec_file(path_segments):
+    """Execute a single python file to get the variables defined in it"""
+    result = {}
+    code = read_file(path_segments)
+    exec(code, result)
+    return result
+
+
+def read_file(path_segments):
+    """Read a file from the package. Takes a list of strings to join to
+    make the path"""
+    file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), *path_segments)
+    with open(file_path) as f:
+        return f.read()
+
+
+long_description = read_file(("README.md",))
+
+
+setup(
+    name="signets",
+    version='0.2.0',
+    description="Un robot qui vous veut du bien",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.31.0",
+     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ signets/signets.py
@@ -0,0 +1,63 @@
+import logging
+import os
+import time
+from interfaces import IObserver, IObservable, IPlugin
+from sqlite_handler import SQLiteHandler
+from interrogationLocale import InterrogationLocale
+
+
+
+logger = logging.getLogger(__name__)
+
+    
+class Signets(IObserver):
+    def __init__(self,observable:IObservable=None):
+        self.__observable = observable
+
+        # Déterminer le répertoire du plugin
+        plugin_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # Chemin du nouveau répertoire 'data'
+        self.data_dir = os.path.join(plugin_dir, 'data')
+
+        # Créer le répertoire de données s'il n'existe pas
+        os.makedirs(self.data_dir, exist_ok=True)
+        db_path = os.path.join(self.data_dir, 'test_context.sqlite')
+        
+        logger.info(f"********************** Répertoire de données du plugin : {self.data_dir}")
+ 
+
+    def f(self,question:str,utilisateur:str,attachments):
+        print(f"Question de {utilisateur} : {question}")            
+        reponse = ""
+        try:
+            stime = time.time()
+        except BaseException as e:
+            print(f"Quelque chose n'a pas fonctionné {e}")
+            reponse = None
+        reponset = f"{time.time()-stime} {question}"
+        return reponset
+
+    async def notify(self,msg:str,to:str,attachments):
+        logger.info(f"***************************** L'utilisateur {to} a écrit {msg}")
+        reponse = self.f(msg,to,attachments)
+        if reponse == None:
+            reponse = "Une erreur s'est produite lors de l'interrogation du LLM"
+        await self.__observable.notify("Coco a dit : "+reponse, to, attachments)
+
+    def prefix(self):
+        return "!f"
+    
+class Plugin(IPlugin):
+    def __init__(self,observable:IObservable):
+        self.__observable = observable
+        self.signets = Signets(self.__observable)
+        # Autres initialisations
+        logger.info(f"********************** Observateur créé {self.signets.prefix()}")
+        
+    def start(self):
+        logger.info(f"********************** Inscripton de {self.signets.prefix()}")
+        self.__observable.subscribe(self.signets)
+
+    async def stop(self):
+        self.__observable.unsubscribe(self.signets)
